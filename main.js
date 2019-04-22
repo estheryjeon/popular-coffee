7b765dd13d5a40702f077cb7c212c0e14e7e0dff
Esther Jeon <estherjeon@modv-ve507-0068.apn.wlan.upenn.edu>
2019-04-22 13:19:36 -0400

    bodhi

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
   var info = document.getElementById("info");
   var btn = document.getElementById("square-one-1");
   btn.onclick = function() {
-    info.innerHTML = "<p>" + "1 " + local_data[0].name + "</p>" + local_data[0].address;
+    info.innerHTML = "<p>" + "1 " + local_data[0].name + "</p>" + "<p>" + local_data[0].address +
+    "</p>X";
     info.style.display = "block";
   }
 
@@ -12,7 +13,8 @@ $(document).ready(function() {
   var hour = today.getHours();
 
   for (i=0; i<local_data.length; i++) {
-    var newOpacity = '0.' + local_data[i].populartimes[day].data[hour];
+    var newOpacity = '0.' + local_data[i].populartimes[day-1].data[hour];
+    console.log(newOpacity);
     $("#" + local_data[i].tag).css('opacity', newOpacity);
   }
 
