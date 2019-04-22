8149633af65005d0f6f4758b6c2ba80d008a8f2b
Esther Jeon <estherjeon@levy-ve504-0488.apn.wlan.upenn.edu>
2019-04-22 11:33:30 -0400

    data update

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
   var local_data = data;
+  var info = document.getElementById("info");
+  var btn = document.getElementById("square-one-1");
+  btn.onclick = function() {
+    info.innerHTML = "<p>" + "1 " + local_data[0].name + "</p>" + local_data[0].address;
+    info.style.display = "block";
+  }
 
   var today = new Date();
-  var weekday = today.getDay();
+  var day = today.getDay();
   var hour = today.getHours();
 
   for (i=0; i<local_data.length; i++) {
-    var newOpacity = '0.' + local_data[i].hours[weekday][hour];
-    $("#" + local_data[i].name).css('opacity', newOpacity);
+    var newOpacity = '0.' + local_data[i].populartimes[day].data[hour];
+    $("#" + local_data[i].tag).css('opacity', newOpacity);
   }
 
 });
