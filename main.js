c9b401488d5b9fca8b954f9fb8409b1238014350
Esther Jeon <estherjeon@Esthers-MacBook-Pro.local>
2019-04-22 23:00:01 -0400

    shop info

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
   var local_data = data;
-  var info = document.getElementById("info");
-  var btn = document.getElementById("square-one-1");
-  btn.onclick = function() {
-    info.innerHTML = "<p>" + "1 " + local_data[0].name + "</p>" + "<p>" + local_data[0].address +
-    "</p>X";
-    info.style.display = "block";
-  }
+  var box = document.getElementById("info-box");
+  var close = document.getElementById("close");
+
+  $('.popular').click(function(e) {
+    var id = $(e.target).prop('id');
+    for (i=0; i<local_data.length; i++) {
+      if (id == local_data[i].tag) {
+        var newText = i + 1 + "\n / " + local_data[i].name + "\n / " + local_data[i].address;
+        $('#info').empty().append(newText);
+        box.style.display = "block";
+        close.onclick = function() {
+          $("#info").empty();
+          box.style.display = "none";
+        }
+      }
+    }
+  });
 
   var today = new Date();
   var day = today.getDay();
   var hour = today.getHours();
 
   for (i=0; i<local_data.length; i++) {
-    var newOpacity = '0.' + local_data[i].populartimes[day-1].data[hour];
-    console.log(newOpacity);
+    var newOpacity = '0.' + local_data[i].populartimes[day].data[hour];
     $("#" + local_data[i].tag).css('opacity', newOpacity);
   }
 
