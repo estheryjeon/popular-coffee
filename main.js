22ef6a5bb7dbbd8ba15a84a332e93e0952f329a7
Esther Jeon <estherjeon@levy-ve504-0909.apn.wlan.upenn.edu>
2019-04-23 11:01:12 -0400

    opacity

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ $(document).ready(function() {
   var box = document.getElementById("info-box");
   var close = document.getElementById("close");
 
+  var today = new Date();
+  var day = today.getDay();
+  var hour = today.getHours();
+
+  for (i=0; i<local_data.length; i++) {
+    var newOpacity = '0.' + local_data[i].populartimes[day].data[hour];
+    $("#" + local_data[i].tag).css('opacity', newOpacity);
+  }
+
   $('.popular').click(function(e) {
     var id = $(e.target).prop('id');
     for (i=0; i<local_data.length; i++) {
@@ -21,14 +30,5 @@ $(document).ready(function() {
   $(window).scroll(function() {
     $('#info-box').css('top', $(this).scrollTop() + "px");
   });
-
-  var today = new Date();
-  var day = today.getDay();
-  var hour = today.getHours();
-
-  for (i=0; i<local_data.length; i++) {
-    var newOpacity = '0.' + local_data[i].populartimes[day].data[hour];
-    $("#" + local_data[i].tag).css('opacity', newOpacity);
-  }
 
 });
